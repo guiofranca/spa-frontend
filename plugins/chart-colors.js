10a16f69e7aa57b0cf1e1d0b4225ad561c8fee78
Guilherme França <dankan282@gmail.com>
2022-01-16 21:06:03 -0300

    Chartjs install

---
+++ plugins/chart-colors.js
@@ -0,0 +1,20 @@
+export default ({ $config: { colorSeed }}, inject ) => {
+    inject('chartColor', {
+        get(array) {
+            return array.map(function(str) {
+                var hash = colorSeed ?? 25000;
+                if (str.length === 0) return hash;
+                for (var i = 0; i < str.length; i++) {
+                    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+                    hash = hash & hash;
+                }
+                var color = '#';
+                for (var i = 0; i < 3; i++) {
+                    var value = (hash >> (i * 8)) & 255;
+                    color += ('00' + value.toString(16)).substr(-2);
+                }
+                return color;
+            })
+        },
+    })
+}
---
+++ plugins/chart.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import { Line, Doughnut } from 'vue-chartjs'
+
+Vue.component('line-chart', {
+	extends: Line,
+	props: ['data', 'options'],
+	mounted () {
+		this.renderChart(this.data, this.options)
+	}
+})
+
+Vue.component('doughnut-chart', {
+	extends: Doughnut,
+	props: ['data', 'options'],
+	mounted () {
+		this.renderChart(this.data, this.options)
+	}
+})
+
